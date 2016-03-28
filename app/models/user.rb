61478dabc2b2fbd3090397efe202c6d8777a36c0
Moses Kim <moseskim92@yahoo.com>
2016-03-28 15:28:46 +0200

    Created all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  belongs_to :profile
+  has_many :bikes
+  has_many :bookings
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
