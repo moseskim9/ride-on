d9fc0b03fed45f7e94cb0f1552df18e2b16a73ca
Moses Kim <moseskim92@yahoo.com>
2016-03-30 09:47:50 +0200

    added some more bike db

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,7 @@ Bike.create(make: 'Solex', :model => '3300', year: '1982', engine: '50cm3', bike
 Bike.create(make: 'Yamaha', :model => 'DT', year: '2005', engine: '125cm3', bike_type: 'motorcycle', rate_daily: 5, bike_image: 'http://dirtrider.net/forums3/data/attachments/11/11857-da6b3a9fa5986b039afccfd83947e579.jpg')
 Bike.create(make: 'BMW', :model => 'nine-T', year: '2014', engine: '1200cm3', bike_type: 'motorcycle', rate_daily: 5, bike_image: 'https://cdn.wheelson.net/photo/2016/02/01/16142948064071049216/14963925164255973376/BMW-R-nine-T-2015-plus-7-year-unlimited-warranty.jpg')
 Bike.create(make: 'Vespa', :model => 'T', year: '2000', engine: '50cm3', bike_type: 'scooter', rate_daily: 5, bike_image: 'http://bikeportal.in/img/new-cars-gallery/vespa/s/desc/New-Vespa-S.jpg')
+Bike.create(make: 'Honda', :model => '600rr', year: '2015', engine: '50cm3', bike_type: 'sport', rate_daily: 5, bike_image: 'https://i.ytimg.com/vi/y0cLVPQmGDk/hqdefault.jpg')
+Bike.create(make: 'Suzuki', :model => 'gsxr1000', year: '2010', engine: '1000', bike_type: 'sport', rate_daily: 5, bike_image: 'http://www.1tail.com/productImages_thumbBIG/180227')
+Bike.create(make: 'Kawasaki', :model => 'Ninja', year: '2000', engine: '600', bike_type: 'sport', rate_daily: 5, bike_image: 'http://www.moto123.com/ArtImages/97273/2008-kawasaki-250R-i01.jpg')
+Bike.create(make: 'Harley-Davidson', :model => 'FL Bobber', year: '1954', engine: '600', bike_type: 'bobber', rate_daily: 5, bike_image: 'https://farm6.staticflickr.com/5020/5480023557_f8fe87755b.jpg')
